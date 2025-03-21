40c27f6911545e1456114b8ba6cc86d83cebf344
winco <foam@winco.com>
2025-03-21 11:02:29 +0100

    debug add restaurant

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,23 +1,3 @@
-// import mongoose from 'mongoose';
-
-// const restaurantSchema = new mongoose.Schema({
-//     name: String,
-//     location: String,
-//     cuisine: String,
-//     menu: [
-//         {
-//             item: String,
-//             price: Number,
-//             description: String,
-//             image: String
-//         }
-//     ]
-// });
-
-// const Restaurant = mongoose.model('Restaurant', restaurantSchema);
-
-// export default Restaurant;
-
 import mongoose from 'mongoose';
 
 const restaurantSchema = new mongoose.Schema({
